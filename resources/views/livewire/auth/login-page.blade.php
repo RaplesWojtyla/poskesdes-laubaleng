b519e02a30de9f6e3f2e7335699c6f3a712d374b
Wojtyla <144253449+RaplesWojtyla@users.noreply.github.com>
2024-12-19 18:47:25 +0700

    Add Show Passwords

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -16,7 +16,7 @@
           <hr class="my-5 border-slate-300">
 
           <!-- Form -->
-          <form wire:submit.prevent="login">
+          <form wire:submit.prevent="login" x-data="{ showPassword: false }">
             @if (session()->has('error'))
             <div class="mt-3 mb-4 bg-red-500 text-sm text-white rounded-lg p-4" role="alert" tabindex="-1" aria-labelledby="hs-solid-color-danger-label">
               {{ session('error') }}
@@ -50,7 +50,16 @@
                   <a wire:navigate class="text-sm text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/forgot">Forgot password?</a>
                 </div>
                 <div class="relative">
-                  <input type="password" id="password" wire:model="password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="password-error">
+                  <input :type="showPassword ? 'text' : 'password'" id="password" wire:model="password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="password-error">
+                  <button type="button" @click="showPassword = !showPassword" class="absolute inset-y-0 end-0 flex items-center pr-3">
+                    <svg :class="{'hidden': showPassword, 'block': !showPassword}" class="h-5 w-5 text-gray-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-.274.823-.68 1.597-1.196 2.285M15 12a3 3 0 01-6 0m6 0a3 3 0 01-6 0m6 0c0 1.657-1.343 3-3 3s-3-1.343-3-3m6 0c0-1.657-1.343-3-3-3s-3 1.343-3 3" />
+                    </svg>
+                    <svg :class="{'block': showPassword, 'hidden': !showPassword}" class="h-5 w-5 text-gray-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7 .274-.823.68-1.597 1.196-2.285M15 12a3 3 0 01-6 0m6 0a3 3 0 01-6 0m6 0c0 1.657-1.343 3-3 3s-3-1.343-3-3m6 0c0-1.657-1.343-3-3-3s-3 1.343-3 3" />
+                    </svg>
+                  </button>
                   @error('password')
                   <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                     <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
--- resources/views/livewire/auth/reset-password-page.blade.php
+++ resources/views/livewire/auth/reset-password-page.blade.php
@@ -9,13 +9,22 @@
 
           <div class="mt-5">
             <!-- Form -->
-            <form wire:submit.prevent="resetPassword">
+            <form wire:submit.prevent="resetPassword" x-data="{ showPassword: false, showConfirmationPassword: false }">
               <div class="grid gap-y-4">
                 <!-- Form Group -->
                 <div>
                   <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
                   <div class="relative">
-                    <input type="password" id="password" wire:model="password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" aria-describedby="email-error">
+                    <input :type="showPassword ? 'text' : 'password'" id="password" wire:model="password" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" aria-describedby="email-error">
+                    <button type="button" @click="showPassword = !showPassword" class="absolute inset-y-0 end-0 flex items-center pr-3">
+                      <svg :class="{'hidden': showPassword, 'block': !showPassword}" class="h-5 w-5 text-gray-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-.274.823-.68 1.597-1.196 2.285M15 12a3 3 0 01-6 0m6 0a3 3 0 01-6 0m6 0c0 1.657-1.343 3-3 3s-3-1.343-3-3m6 0c0-1.657-1.343-3-3-3s-3 1.343-3 3" />
+                      </svg>
+                      <svg :class="{'block': showPassword, 'hidden': !showPassword}" class="h-5 w-5 text-gray-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7 .274-.823.68-1.597 1.196-2.285M15 12a3 3 0 01-6 0m6 0a3 3 0 01-6 0m6 0c0 1.657-1.343 3-3 3s-3-1.343-3-3m6 0c0-1.657-1.343-3-3-3s-3 1.343-3 3" />
+                      </svg>
+                    </button>
                     @error('password')
                     <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                       <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
@@ -33,7 +42,16 @@
                 <div>
                   <label for="password_confirmation" class="block text-sm mb-2 dark:text-white">Confirm Password</label>
                   <div class="relative">
-                    <input type="password" id="password_confirmation" wire:model="password_confirmation" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" aria-describedby="email-error">
+                    <input :type="showConfirmationPassword ? 'text' : 'password'" id="password_confirmation" wire:model="password_confirmation" class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" aria-describedby="email-error">
+                    <button type="button" @click="showConfirmationPassword = !showConfirmationPassword" class="absolute inset-y-0 end-0 flex items-center pr-3">
+                      <svg :class="{'hidden': showConfirmationPassword, 'block': !showConfirmationPassword}" class="h-5 w-5 text-gray-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-.274.823-.68 1.597-1.196 2.285M15 12a3 3 0 01-6 0m6 0a3 3 0 01-6 0m6 0c0 1.657-1.343 3-3 3s-3-1.343-3-3m6 0c0-1.657-1.343-3-3-3s-3 1.343-3 3" />
+                      </svg>
+                      <svg :class="{'block': showConfirmationPassword, 'hidden': !showConfirmationPassword}" class="h-5 w-5 text-gray-500" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7 .274-.823.68-1.597 1.196-2.285M15 12a3 3 0 01-6 0m6 0a3 3 0 01-6 0m6 0c0 1.657-1.343 3-3 3s-3-1.343-3-3m6 0c0-1.657-1.343-3-3-3s-3 1.343-3 3" />
+                      </svg>
+                    </button>
                     @error('password_confirmation')
                     <div class="inset-y-0 end-0 flex items-center pointer-events-none pe-3">
                       <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
